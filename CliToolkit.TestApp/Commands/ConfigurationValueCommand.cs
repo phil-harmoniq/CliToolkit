15a68481734347f15d15d94ff9344885479f0300
Phil Hawkins <phil.harmoniq@me.com>
2020-06-24 22:15:51 -0400

    More prep

---
+++ CliToolkit.TestApp/Commands/ConfigurationValueCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace CliToolkit.TestApp.Commands
+{
+    public class ConfigurationValueCommand : CliCommand<ConfigurationValueOptions>
+    {
+        protected override void OnExecute(ConfigurationValueOptions options, string[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class ConfigurationValueOptions
+    {
+
+    }
+}
