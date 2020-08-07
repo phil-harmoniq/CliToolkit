7a2ef33e527b042402102cefc1750faca0886ace
Phil Hawkins <phil.harmoniq@me.com>
2020-08-06 22:03:37 -0400

    Drop unneeded IOptions dependency

--- CliToolkit/Internal/Extensions.cs
+++ CliToolkit/Internal/Extensions.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -11,14 +6,6 @@ namespace CliToolkit.Internal
 {
     internal static class Extensions
     {
-        internal static IServiceCollection AddConfig<TConfig>(
-            this IServiceCollection services, IConfiguration config) where TConfig : class, new()
-        {
-            services.Configure<TConfig>(config.GetSection(typeof(TConfig).Name));
-            services.AddScoped(sp => sp.GetRequiredService<IOptions<TConfig>>().Value);
-            return services;
-        }
-
         internal static bool HasPublicSetter(this PropertyInfo prop)
         {
             return prop.CanWrite && prop.GetSetMethod(true).IsPublic;
